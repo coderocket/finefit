58c137ac3c497f8dbd6ec8bfdf3024c2976c74bd
coderocket <david.faitelson@gmail.com>
2014-08-10 13:02:51 +0300

    implemented (buggy on purpose) login demo

--- demos/login/Login.java
+++ demos/login/Login.java
@@ -6,6 +6,7 @@ import java.util.Map;
 import java.util.HashMap;
 import java.util.Map.Entry;
 import java.util.Set;
+import java.util.HashSet;
 import kodkod.ast.Relation;
 import kodkod.instance.Instance;
 import kodkod.instance.Tuple;
@@ -16,15 +17,29 @@ import com.finefit.testcasegenerator.StateVariables;
 
 public class Login {
 
-	public void init() {}
+	Set<String> registered;
+	Set<String> loggedIn;
+	
+	public void init() {
+		registered = new HashSet<String>();
+		loggedIn = new HashSet<String>();
+	}
 
-	public void login(String uname) {}
+	public void login(String uname) {
+		loggedIn.add(uname);
+	}
 
-	public void logout(String uname) {}
+	public void logout(String uname) {
+		loggedIn.remove(uname);
+	}
 
-	public void register(String uname) {}
+	public void register(String uname) {
+		registered.add(uname);
+	}
 
-	public void unregister(String uname) {}
+	public void unregister(String uname) {
+		registered.remove(uname);
+	}
 
 	public Instance retrieve(Universe universe, StateVariables stateVars) {
 		TupleFactory factory = universe.factory();
@@ -33,6 +48,13 @@ public class Login {
 		List<Tuple> loggedinTuples = new ArrayList();
 		List<Tuple> registeredTuples = new ArrayList();
 
+		for (String s : loggedIn) {
+			loggedinTuples.add(factory.tuple("State$0", s));
+		}
+		for (String s : registered) {
+			registeredTuples.add(factory.tuple("State$0", s));
+		}
+		
 		if(!loggedinTuples.isEmpty()){
 			instance.add(stateVars.get("loggedin"), factory.setOf(loggedinTuples));
 		}else {
