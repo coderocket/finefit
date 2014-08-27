e929bcec45265bcdaa947ea1a7acc13f307aa850
coderocket <david.faitelson@gmail.com>
2014-08-27 15:09:00 +0300

    fixed typos

--- src/com/finefit/translator/Constants.java
+++ src/com/finefit/translator/Constants.java
@@ -5,4 +5,5 @@ public class Constants {
 	public static final String STATE_SIG = "State";
 	public static final String CURR_VAR = "s";
   public static final String NEXT_VAR = "s'";
+  public static final String OUTPUT_PREFIX = "output_";
 }
