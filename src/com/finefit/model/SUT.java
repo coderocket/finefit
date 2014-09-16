a7e965bb3b6f5f941dbd452ef87aba6068719299
coderocket <david.faitelson@gmail.com>
2014-09-16 15:28:34 +0300

    added a message option to the NoSuchOperationException exception

--- src/com/finefit/model/SUT.java
+++ src/com/finefit/model/SUT.java
@@ -22,7 +22,11 @@ package com.finefit.model;
 public interface SUT {
 	 
 		public class InvalidNumberOfArguments extends Exception {}
-    public class NoSuchOperation extends Exception {}
+    public class NoSuchOperation extends Exception { 
+			public NoSuchOperation(String msg) {
+				super(msg);
+			}
+		}
 
     public State initialize(State state);
     public State applyOperation(TestCase testCase) throws InvalidNumberOfArguments, NoSuchOperation;
